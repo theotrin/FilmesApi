bc98937ea6e0060b51dbdf9ebcf248ae51be4d45
19900 <19900@TREINAR510_08>
2024-09-02 11:23:38 -0400

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,31 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+    private static int id = 0;
+
+    [HttpPost]
+    public void AdcionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> VerFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? VerFilmePorId(int id)
+    {
+       return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
